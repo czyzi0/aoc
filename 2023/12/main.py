b0fcfc379afb5a5c514977cdee0f4f2172e26fe8
czyzi0 <czyznikiewicz.mateusz@gmail.com>
2025-01-03 19:20:01 +0100

    Add not good enough solution for day 12 year 2023

--- 2023/12/main.py
+++ 2023/12/main.py
@@ -6,9 +6,10 @@ SOLUTIONS_PART1 = {
     "sample1.txt": 21,
     "input.txt": 6958,
 }
-# SOLUTIONS_PART2 = {
-#     "input.txt": 544723432977,
-# }
+SOLUTIONS_PART2 = {
+    "sample1.txt": 525152,
+    "input.txt": -1,
+}
 
 
 def solve(file_path):
@@ -20,66 +21,66 @@ def solve(file_path):
         solution_part1 = SOLUTIONS_PART1[file_path.name]
         assert solution_part1_ == solution_part1
 
-    # if file_path.name in SOLUTIONS_PART2:
-    #     solution_part2_ = solve_part2(file_path)
-    #     print(f"Part 2: {solution_part2_}")
-    #     solution_part2 = SOLUTIONS_PART2[file_path.name]
-    #     assert solution_part2_ == solution_part2
+    if file_path.name in SOLUTIONS_PART2:
+        solution_part2_ = solve_part2(file_path)
+        print(f"Part 2: {solution_part2_}")
+        solution_part2 = SOLUTIONS_PART2[file_path.name]
+        assert solution_part2_ == solution_part2
 
 
 def solve_part1(file_path):
-
-    def is_ok(candidate, limitation):
-        for chr_candidate, chr_limitation in zip(candidate, limitation):
-            if chr_limitation != "?" and chr_limitation != chr_candidate:
-                return False
-        return True
-
-    limitations, groups_list = [], []
-    with open(file_path, "r", encoding="utf-8") as f:
-        for line in f:
-            limitation, groups = line.strip().split()
-            limitations.append(limitation)
-            groups_list.append([int(g) for g in groups.split(",")])
-
     result = 0
-    for limitation, groups in zip(limitations, groups_list):
-        for candidate in generate_candidates(groups, len(limitation)):
-            if is_ok(candidate, limitation):
-                result += 1
-
+    for pattern, expected_groups in load_lines(file_path, n_folds=1):
+        result += count_possible_solutions(pattern, expected_groups)
     return result
 
 
-# def solve_part2(file_path):
-#     return 0
+def solve_part2(file_path):
+    result = 0
+    for pattern, expected_groups in load_lines(file_path, n_folds=5):
+        result += count_possible_solutions(pattern, expected_groups)
+    return result
 
 
-def generate_candidates(groups, length):
-    n = length - sum(groups) - len(groups) + 1
-    k = len(groups) + 1
-    for gaps in generate_k_compositions(n, k):
-        gaps = [gaps[0], *[g + 1 for g in gaps[1:-1]], gaps[-1]]
-        yield "".join((
-            *["." * gap + "#" * group for gap, group in zip(gaps[:-1], groups)],
-            "." * gaps[-1]
-        ))
+def load_lines(file_path, n_folds):
+    with open(file_path, "r", encoding="utf-8") as f:
+        for line in f:
+            pattern, groups = line.strip().split()
+
+            groups = [int(g) for g in reversed(",".join([groups] * n_folds).split(","))]
+            pattern = "?".join([pattern] * n_folds) + "."
+            yield pattern, groups
 
 
-def generate_k_compositions(n, k):
-    composition = [0] * k
-    composition[0] = n
-    while True:
-        yield composition
-        v = composition[-1]
-        if v == n:
-            break
-        composition[-1] = 0
-        j = -2
-        while composition[j] == 0:
-            j -= 1
-        composition[j] -= 1
-        composition[j + 1] = 1 + v
+def count_possible_solutions(pattern, groups):
+    result = 0
+
+    def do(pattern, groups):
+        if not pattern and not groups:
+            nonlocal result
+            result += 1
+            return
+
+        success, new_pattern = accept(pattern, ".")
+        if success and sum(groups) + len(groups) <= len(new_pattern):
+            do(new_pattern, groups)
+
+        if not groups:
+            return
+        group = groups.pop()
+        success, new_pattern = accept(pattern, "#" * group + ".")
+        if success and sum(groups) + len(groups) <= len(new_pattern):
+            do(new_pattern, groups)
+        groups.append(group)
+
+    def accept(pattern, prefix):
+        for prefix_chr, pattern_chr in zip(prefix, pattern):
+            if pattern_chr not in {"?", prefix_chr}:
+                return False, ""
+        return True, pattern[len(prefix):]
+
+    do(pattern, groups)
+    return result
 
 
 if __name__ == "__main__":
