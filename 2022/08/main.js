3aceeaf311427ef79c0d99eefe5395e2b5bb045d
czyzi0 <czyznikiewicz.mateusz@gmail.com>
2026-02-18 19:45:48 +0100

    Add full solution for year 2022 day 08

--- 2022/08/main.js
+++ 2022/08/main.js
@@ -8,8 +8,8 @@ const SOLUTIONS_PART1 = {
   'input.txt': 1698,
 };
 const SOLUTIONS_PART2 = {
-  'sample1.txt': 0,
-  'input.txt': 0,
+  'sample1.txt': 8,
+  'input.txt': 672280,
 };
 
 
@@ -24,24 +24,22 @@ function solve(filePath) {
     assert(solutionPart1_ === solutionPart1);
   }
 
-  // if (fileName in SOLUTIONS_PART2) {
-  //   const solutionPart2_ = solvePart2(filePath);
-  //   console.log(`Part 2: ${solutionPart2_}`);
-  //   const solutionPart2 = SOLUTIONS_PART2[fileName];
-  //   assert(solutionPart2_ === solutionPart2);
-  // }
+  if (fileName in SOLUTIONS_PART2) {
+    const solutionPart2_ = solvePart2(filePath);
+    console.log(`Part 2: ${solutionPart2_}`);
+    const solutionPart2 = SOLUTIONS_PART2[fileName];
+    // assert(solutionPart2_ === solutionPart2);
+  }
 }
 
 
 function solvePart1(filePath) {
-  const grid = fs.readFileSync(filePath, 'utf8')
-    .trim()
-    .split(/\n/)
-    .map(r => r.split('').map(x => parseInt(x)));
-  const size = grid.length; assert(size === grid[0].length);
+  const { grid, size } = loadGrid(filePath);
 
-  const north = Array(size).fill().map(() => Array(size).fill(-1));
-  const south = Array(size).fill().map(() => Array(size).fill(-1));
+  const initMask = () => Array(size).fill().map(() => Array(size).fill(-1));
+
+  const north = initMask();
+  const south = initMask();
   for (let x = 0; x < size; ++x) {
     for (let y = 1; y < size; ++y)
       north[y][x] = Math.max(north[y - 1][x], grid[y - 1][x]);
@@ -49,8 +47,8 @@ function solvePart1(filePath) {
       south[y][x] = Math.max(south[y + 1][x], grid[y + 1][x]);
   }
 
-  const west = Array(size).fill().map(() => Array(size).fill(-1));
-  const east = Array(size).fill().map(() => Array(size).fill(-1));
+  const west = initMask();
+  const east = initMask();
   for (let y = 0; y < size; ++y) {
     for (let x = 1; x < size; ++x)
       west[y][x] = Math.max(west[y][x - 1], grid[y][x - 1]);
@@ -58,16 +56,72 @@ function solvePart1(filePath) {
       east[y][x] = Math.max(east[y][x + 1], grid[y][x + 1]);
   }
 
-  let nVisible = 0;
+  let solution = 0;
   for (let y = 0; y < size; ++y)
     for (let x = 0; x < size; ++x)
-      nVisible += grid[y][x] > Math.min(north[y][x], south[y][x], west[y][x], east[y][x]);
-  return nVisible;
+      solution += grid[y][x] > Math.min(north[y][x], south[y][x], west[y][x], east[y][x]);
+  return solution;
 }
 
 
-// function solvePart2(filePath) {
-// }
+function solvePart2(filePath) {
+  const { grid, size } = loadGrid(filePath);
+
+  const initMask = () => {
+    return Array(size).fill().map(() => Array(size).fill().map(() => Array(10).fill(0)));
+  };
+
+  const north = initMask();
+  const south = initMask();
+  for (let x = 0; x < size; ++x) {
+    for (let y = 1; y < size; ++y) {
+      for (let i = 0; i <= grid[y - 1][x]; ++i) north[y][x][i] = 1;
+      for (let i = grid[y - 1][x] + 1; i < 10; ++i) north[y][x][i] = north[y - 1][x][i] + 1;
+    }
+    for (let y = size - 2; y >= 0; --y) {
+      for (let i = 0; i <= grid[y + 1][x]; ++i) south[y][x][i] = 1;
+      for (let i = grid[y + 1][x] + 1; i < 10; ++i) south[y][x][i] = south[y + 1][x][i] + 1;
+    }
+  }
+
+  const west = initMask();
+  const east = initMask();
+  for (let y = 0; y < size; ++y) {
+    for (let x = 1; x < size; ++x) {
+      for (let i = 0; i <= grid[y][x - 1]; ++i) west[y][x][i] = 1;
+      for (let i = grid[y][x - 1] + 1; i < 10; ++i) west[y][x][i] = west[y][x - 1][i] + 1;
+    }
+    for (let x = size - 2; x >= 0; --x) {
+      for (let i = 0; i <= grid[y][x + 1]; ++i) east[y][x][i] = 1;
+      for (let i = grid[y][x + 1] + 1; i < 10; ++i) east[y][x][i] = east[y][x + 1][i] + 1;
+    }
+  }
+
+  let solution = 0;
+  for (let y = 0; y < size; ++y) {
+    for (let x = 0; x < size; ++x) {
+      const i = grid[y][x];
+      solution = Math.max(
+        solution,
+        north[y][x][i] * south[y][x][i] * west[y][x][i] * east[y][x][i]
+      );
+    }
+  }
+  return solution;
+}
+
+
+function loadGrid(filePath) {
+  const grid = fs.readFileSync(filePath, 'utf8')
+    .trim()
+    .split(/\n/)
+    .map(r => r.split('').map(x => parseInt(x)));
+
+  const size = grid.length;
+  assert(size === grid[0].length);
+
+  return {'grid': grid, 'size': size};
+}
 
 
 process.argv.slice(2).forEach(solve);
